e71e5ac0916079e56d0d21d2ae7f009256cfd6d3
babucse1991 <babucse1991@gmail.com>
2018-08-02 01:06:13 +0530

    Chanfes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import {
-  GoogleMap,
-  GoogleMaps,
-  GoogleMapsEvent,
-  Marker,
-  GoogleMapsAnimation,
-  MyLocation
-} from '@ionic-native/google-maps';
+import { ViewChild } from '@angular/core';
+import {} from '@types/googlemaps';
 import { NavController } from 'ionic-angular';
 import { TruckFilterPage } from '../truck-filter/truck-filter';
 
@@ -17,76 +11,23 @@ import { TruckFilterPage } from '../truck-filter/truck-filter';
 })
 export class HomePage {
 
-  mapReady: boolean = false;
-  map: GoogleMap ;
-
-
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+  
   constructor(public navCtrl: NavController) {
+  
   }
-
-  ionViewDidLoad() {
-    this.loadMap();
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
-
-  loadMap() {
-    // Create a map after the view is loaded.
-    // (platform is already ready in app.component.ts)
-    this.map = GoogleMaps.create('map_canvas', {
-      camera: {
-        target: {
-          lat: 43.0741704,
-          lng: -89.3809802
-        },
-        zoom: 18,
-        tilt: 30
-      }
-    });
-
-    // Wait the maps plugin is ready until the MAP_READY event
-    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
-      this.mapReady = true;
-    });
-  }
-
-  onButtonClick() {
-    if (!this.mapReady) {
-      this.showToast('map is not ready yet. Please try again.');
-      return;
-    }
-    this.map.clear();
-
-    // Get the location of you
-    this.map.getMyLocation()
-      .then((location: MyLocation) => {
-        console.log(JSON.stringify(location, null ,2));
-
-        // Move the map camera to the location with animation
-        return this.map.animateCamera({
-          target: location.latLng,
-          zoom: 17,
-          tilt: 30
-        }).then(() => {
-          // add a marker
-          return this.map.addMarker({
-            title: '@ionic-native/google-maps plugin!',
-            snippet: 'This plugin is awesome!',
-            position: location.latLng,
-            animation: GoogleMapsAnimation.BOUNCE
-          });
-        })
-      }).then((marker: Marker) => {
-        // show the infoWindow
-        marker.showInfoWindow();
-
-        // If clicked it, display the alert
-        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-          this.showToast('clicked!');
-        });
-      });
-  }
-
-  showToast(message: string) {
-    alert(message);
+  
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)    
   }
 
   goToTruckFilter(params){
