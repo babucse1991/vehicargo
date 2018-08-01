5ef1b91da3d1006424ed1b2a7564ab945db62283
Babu S <Babu.S@mphasis.com>
2018-08-01 17:43:44 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,11 @@ import { WelcomePage } from '../pages/welcome/welcome';
 })
 export class MyApp {
   
-  rootPage:any = WelcomePage;
+  rootPage:any ;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      this.rootPage = WelcomePage;
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
